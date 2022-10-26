86ba9f80dd96faa62e0c4acb40df6e922853b50f
hyungjin099 <hyungjin099@gmail.com>
2022-10-26 19:14:31 +0900

    첫 테스트

---
+++ src/main/java/kh/study/gitTest/GuiTestApplication.java
@@ -0,0 +1,13 @@
+package kh.study.gitTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuiTestApplication.class, args);
+	}
+
+}
